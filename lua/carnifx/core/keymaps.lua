b24fa4b56333f355e9ec2c061c159036d1e72894
Josh <josh.jacobsen@cogo.co>
2023-02-10 11:20:09 +1300

    Add shortcut to highlight word occurance and show file in nvim-tree

--- lua/carnifx/core/keymaps.lua
+++ lua/carnifx/core/keymaps.lua
@@ -40,6 +40,7 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window max
 
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>lf", ":NvimTreeFindFile<CR>") -- Locate current file in explorer
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
@@ -47,3 +48,5 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+keymap.set("n", "ss", "*") -- find all instances of variable in current file
