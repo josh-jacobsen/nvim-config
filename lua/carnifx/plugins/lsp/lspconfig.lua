8456b331c2075b2c77a83a5be27819f28fcefa4b
Josh <josh.jacobsen@cogo.co>
2023-04-22 22:33:59 +1200

    remove keymap comments from previous experimentation

--- lua/carnifx/plugins/lsp/lspconfig.lua
+++ lua/carnifx/plugins/lsp/lspconfig.lua
@@ -37,34 +37,18 @@ local on_attach = function(client, bufnr)
 	keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show lspsaga table with references, definition, and implementation
 	keymap.set("n", "gi", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 	keymap.set("n", "gj", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
-
-	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
-	-- I think I like the Lspsaga code actions dialogue more than the built in one
-	-- keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions
-
-	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
-	-- it would appear that this doesn't work
-	-- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- got to declaration
-
-	--keymap.set("n", "gD", vim.lsp.buf.definition, opts) -- got to declaration
+	keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- go to definition of the type, if only 1, otherwise show all options
 
 	-- I actually think I want to use the inbuilt commands for going to definition. Telescope keeps losing my place, so I can't retrace my steps with Ctrl O. This might
 	-- not be a telescope thing but I want to experiment anywat
 	-- keymap.set("n", "gf", "<cmd>Telescope lsp_definitions<CR>", opts) -- see definition and make edits in window
 	nmap("gf", vim.lsp.buf.definition, "[G]oto [D]efinition")
-	-- i dont think gi actually works
-	-- keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- go to implementation
-	keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- go to definition of the type, if only 1, otherwise show all options
-	-- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-	-- keymap.set(
-	-- 	"n",
-	-- 	"gp",
-	-- 	"<cmd>Telescope lsp_type_definitions<CR>",
-	-- 	{ noremap = true, silent = true, buffer = bufnr, jump_type = "never" }
-	-- ) -- go to definition of the type, if only 1, otherwise show all options
+
+	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
 
 	keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-	keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+	keymap.set("n", "<leader>df", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 	keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 	keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 	keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
