99265f836b1362bd6f0d05f6ba1ceabded7c4886
Josh <josh.jacobsen@cogo.co>
2023-06-15 12:33:28 +1200

    try adding support for yaml files (for openapi specs); didn't work

--- lua/carnifx/plugins/lsp/lspconfig.lua
+++ lua/carnifx/plugins/lsp/lspconfig.lua
@@ -129,6 +129,35 @@ lspconfig["pyright"].setup({
 	on_attach = on_attach,
 })
 
+lspconfig["yamlls"].setup({
+	on_attach = on_attach,
+	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	settings = {
+		yaml = {
+			schemas = {
+				["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"] = "conf/**/*catalog*",
+				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+				["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+			},
+		},
+	},
+})
+
+-- lspconfig["yamlls"].setup({})
+-- require("lspconfig").yamlls.setup({
+-- 	on_attach = on_attach,
+-- 	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+-- 	settings = {
+-- 		yaml = {
+-- 			schemas = {
+-- 				["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"] = "conf/**/*catalog*",
+-- 				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+-- 				["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+-- 			},
+-- 		},
+-- 	},
+-- })
+
 -- configure lua server (with special settings)
 lspconfig["lua_ls"].setup({
 	capabilities = capabilities,
