6a9922faf9d7ff74f16856ad7b3fbd58019a96cc
carnifx <carnifx@multitudess-MacBook-Pro.local>
2024-01-05 13:51:30 +1300

    Add option to open terminal either floating or at bottom of window

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -2,4 +2,5 @@ require("toggleterm").setup({
 	direction = "float",
 })
 -- -- terminal
-vim.keymap.set("n", "<leader>ter", ":ToggleTerm<CR>")
+vim.keymap.set("n", "<leader>tl", ":ToggleTerm direction=horizontal size-15<CR>")
+vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
